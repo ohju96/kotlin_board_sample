4cd46b15a5e37d8488a45d8e5c1c25c23106b3ff
ohju96 <ohju96@naver.com>
2023-09-17 19:33:27 +0900

    set

---
+++ java-kotlin/src/main/kotlin/com/example/javakotlin/JavaKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.javakotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JavaKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<JavaKotlinApplication>(*args)
+}
---
+++ java-kotlin/src/test/kotlin/com/example/javakotlin/JavaKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.javakotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JavaKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
